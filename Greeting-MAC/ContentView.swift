4ec5cb885486a609a65d15bc29881dd9ed035cf3
Nour <mohamednour25@gmail.com>
2024-08-28 20:43:19 +0300

    88

---
+++ Greeting-MAC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Greeting-MAC
+//
+//  Created by administrator on 28/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Greeting-MAC/Greeting_MACApp.swift
@@ -0,0 +1,17 @@
+//
+//  Greeting_MACApp.swift
+//  Greeting-MAC
+//
+//  Created by administrator on 28/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Greeting_MACApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
